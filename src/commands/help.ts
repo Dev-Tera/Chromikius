4336fc2f81887f643897d8e7ae9c65ebe2cce710
Dev-Paxton <benedikt.wutzke@gmail.com>
2026-07-02 16:50:44 +0200

    Simplify help command
    Old implementation doesn't give more value than the command selection in
    discord

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,143 +1,20 @@
-import { EmbedBuilder, SlashCommandBuilder, TextChannel, User } from "discord.js";
-import fs from "fs"
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import Command from "../structures/Command";
 
 export default new Command({
     data: new SlashCommandBuilder()
         .setName("help")
-        .setDescription("Antwortet mit der Hilfe zu den Commands")
-        .addStringOption((opt) => opt.setName("command")
-                        .setDescription("Der Command zu dem du hilfe möchtest")),
+        .setDescription("Gibt eine Übersicht über die Commands"),
     userPermissions: [],
     botPermissions: [],
 	allowDm: true,
     execute: async (client, interaction) => {
-		const commandOption = interaction.options.get("command")
+        const embed = new EmbedBuilder().setTitle("Commands").setColor("#ff9e00")
 
-		if(commandOption) {
-			var input_command = commandOption.value as any
-		} else {
-			var input_command = null
-		}
+        for (const [commandName, command] of client.commands) {
+            embed.addFields({ name: commandName, value: command.data.description })
+        }
 
-        var rawdata = fs.readFileSync("../../assets/command-descriptions.json").toString()
-    	var commands = JSON.parse(rawdata)
-
-		if (input_command === null) {
-			const embed = new EmbedBuilder()
-			.setColor("#ff9e00")
-			.setTitle("Commands")
-			.setDescription("Gebe `help [command]` ein oder reagiere auf die Nachricht um mehr Informationen über einen Command zu erhalten")
-
-			const cmd_group_emojis = {
-                "user_cmds": "👤",
-				"level_cmds": "⬆",
-                "mod_cmds": "👮",
-				"warn_cmds": "⚠",
-				"admin_cmds": "🅰️"
-            }
-
-			for (cmd_group in commands) {
-				var cmd_group_name = cmd_group.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())))
-				cmd_group_name = cmd_group_name.replace("_", " ")
-				cmd_group_name = cmd_group_name.replace("cmds", "Commands")
-				cmd_group_name = cmd_group_emojis[String(cmd_group)] + " | " + cmd_group_name
-			
-				var value = ""
-				for (command in commands[String(cmd_group)]) {
-					value += command + "\n"
-				}
-
-				value = "```\n" + value + "```"
-
-				embed.addFields({ name: cmd_group_name, value: value, inline: true })
-			}
-
-			const msg = (await interaction.reply({ embeds: [embed], fetchReply: true }))
-
-			const channel = await client.channels.fetch(msg.channelId) as TextChannel
-			const message = await channel.messages.fetch(msg.id)
-
-			const allowed_reactions = []
-			Object.keys(cmd_group_emojis).forEach(key => {
-				allowed_reactions.push(cmd_group_emojis[key])
-			})
-			
-			msg.react("👤")
-			.then(() => msg.react("⬆")).catch((error) => { if(error.code === 10008) return })
-			.then(() => msg.react("👮")).catch((error) => { if(error.code === 10008) return })
-			.then(() => msg.react("⚠")).catch((error) => { if(error.code === 10008) return })
-			.then(() => msg.react("🅰️")).catch((error) => { if(error.code === 10008) return })
-			
-			const filter = (reaction, user: User) => {
-				if (user.id === interaction.user.id) {
-					if (allowed_reactions.includes(reaction.emoji.name)) {
-						return true
-					}
-				}
-
-				return false
-			}
-
-			msg.awaitReactions({ filter, max: 1, time: 1000 * 30, errors: ["time"] })
-			.then(collected => {
-				const reaction = collected.first()
-				msg.delete()
-
-				const reversed_cmd_group_emojis = {}
-				Object.keys(cmd_group_emojis).forEach(key => {
-					reversed_cmd_group_emojis[cmd_group_emojis[key]] = key
-				})
-
-				const cmd_group = reversed_cmd_group_emojis[reaction.emoji.name]
-
-				var cmd_group_name = cmd_group.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())))
-				cmd_group_name = cmd_group_name.replace("_", " ")
-				cmd_group_name = cmd_group_name.replace("cmds", "Commands")
-				cmd_group_name = cmd_group_emojis[String(cmd_group)] + " | " + cmd_group_name
-
-				var description =""
-				for (var command in commands[cmd_group]) {
-					description += `**${command}**\n${commands[cmd_group][command]}\n\n`
-				
-				}
-				const embed = new EmbedBuilder()
-					.setColor("#ff9e00")
-					.setTitle(cmd_group_name)
-					.setDescription(description)
-				interaction.channel.send({ embeds: [embed] })
-			})
-			.catch(collected => {
-				msg.reactions.removeAll()
-					.catch(error => {
-						if (error.code === 50003) return
-						else console.error('Failed to clear reactions:', error)
-				});
-			});
-		} else {
-			var command_name = null
-			var command_description = null
-			for (var cmd_group in commands) {
-				for (var command in commands[cmd_group]) {
-					if (command.toLowerCase().split(" ")[0] === input_command.toLowerCase()) {
-						command_name = command.split(" ")[0]
-						command_description = commands[cmd_group][command]
-					}
-				}
-			}
-
-			const embed = new EmbedBuilder()
-
-			if (command_name === null && command_description === null) {
-				embed.setColor("#fc030b")
-					.setTitle(`Der Command ${"`" + input_command + "`"} existiert nicht`)
-			} else {
-				embed.setColor("#ff9e00")
-				.setTitle(command_name)
-				.setDescription(command_description)
-			}
-
-			await interaction.reply({ embeds: [embed] })
-		}
+        interaction.reply({ embeds: [embed] })
     }
 })
